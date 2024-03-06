43755f3d1b83c92274cf5779ac31e70c10b8912a
Anand Vishwakarma <anand23197@gmail.com>
2024-03-06 08:47:38 +0530

    changes

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,51 +1,59 @@
-const CustomError = require("../utils/customError")
+const CustomError = require("../utils/customError");
 
-const devErrors = (res,err)=>{
+const devErrors = (res, err) => {
+  res.status(err.statusCode).json({
+    status: err.statusCode,
+    message: err.message,
+    stackTrace: err.stack,
+    error: err,
+  });
+};
+
+const duplicateKeyErrorHandler = (err) => {
+  const name = err.keyValue.name;
+  const msg = `There is already a movie with name ${name}. Please use another name!`;
+  return new CustomError(msg, 400);
+};
+const prodErrors = (res, err) => {
+  if (err.isOperational) {
     res.status(err.statusCode).json({
-        status : err.statusCode,
-        message : err.message,
-        stackTrace : err.stack,
-        error : err
-    })
-}
+      status: err.statusCode,
+      message: err.message,
+    });
+  } else {
+    res.status(500).json({
+      status: "error",
+      message: "something went wrong please try again later",
+    });
+  }
+};
 
-const duplicateKeyErrorHandler = (err)=>{
-    
-     const name = err.keyValue.name;
-     const msg = `There is already a movie with name ${name}. Please use another name!`;
-    return new CustomError(msg,400);
-}
-const prodErrors = (res, err)=>{
-    if(err.isOperational){
-        res.status(err.statusCode).json({
-            status : err.statusCode,
-            message : err.message
-        })
-    }else{
-        res.status(500).json({
-            status : 'error',
-            message : "something went wrong please try again later"
-        })
-    } 
-}
+const castErrorHandler = (err) => {
+  const msg = `Invalid value for ${err.value}: ${err.path}!`;
+  return new CustomError(msg, 400);
+};
 
-const castErrorHandler = (err)=>{
-    const msg = `Invalid value for ${err.value}: ${err.path}!`
-    return new CustomError(msg, 400);
-}
+const validationErrorHandler = (err) => {
+  const errors = Object.values(err.errors).map((val) => val.message);
+  const errorMessages = errors.join(". ");
+  const message = `Invalid input data: ${errorMessages}`;
 
-module.exports = (err, req, res, next)=>{
-    err.statusCode = err.statusCode || 500
-    err.status = err.status || "error"
+  return new CustomError(msg, 400);
+};
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
 
-    if(process.env.NODE_ENV === 'development'){
-        devErrors(res, err);      
-    }else if(process.env.NODE_ENV === 'production'){
-        // let error = {...err};
-        if(err.name === "CastError"){
-            err = castErrorHandler(err)
-        }
-        if(err.code === 11000 ) err=> duplicateKeyErrorHandler(err)
-        prodErrors(res, err);
+  if (process.env.NODE_ENV === "development") {
+    devErrors(res, err);
+  } else if (process.env.NODE_ENV === "production") {
+    // let error = {...err};
+    if (err.name === "CastError") {
+      err = castErrorHandler(err);
     }
-}
+    if (err.code === 11000) err = duplicateKeyErrorHandler(err);
+    if (err.name === "ValidationError") err = validationErrorHandler(err);
+
+    prodErrors(res, err);
+  }
+};
