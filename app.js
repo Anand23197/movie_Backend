7077865243f76cfd4276e25d1fe3bce1a31f0a66
Anand Vishwakarma <anand23197@gmail.com>
2024-03-17 15:00:15 +0530

    changes

--- app.js
+++ app.js
@@ -6,14 +6,47 @@ const authRouter = require("./Routes/authRoutes");
 const userRouter = require("./Routes/userRoutes");
 const CustomError = require("./utils/customError");
 const globalErrHandler = require("./controller/errorController");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const sanitize = require("express-mongo-sanitize");
+const { xss } = require("express-xss-sanitizer");
+const hpp = require("hpp");
 
+app.use(helmet());
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 15 minutes
+  limit: 1000, // Limit each IP to 1000 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  message:
+    "we have received to many request from this ip. please try after 1 hour",
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
+// Apply the rate limiting middleware to all requests.
+app.use("/api", limiter);
 const logger = function (req, res, next) {
   console.log("custom middleware called");
   next();
 };
 
-app.use(express.json());
-
+app.use(express.json({ limit: "10kb" }));
+app.use(sanitize());
+app.use(xss());
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratings",
+      "releaseYear",
+      "ReleaseDate",
+      "genres",
+      "directors",
+      "actors",
+      "price",
+    ],
+  })
+);
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
