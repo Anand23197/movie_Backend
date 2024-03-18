9be5b39c1a7e1b22ea87e9c63611346c6f9367ad
Anand Vishwakarma <anand23197@gmail.com>
2024-03-18 19:39:15 +0530

    changes

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const hpp = require("hpp");
 
 app.use(helmet());
 const limiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 15 minutes
+  windowMs: 60 * 60 * 1000, // 60 minutes
   limit: 1000, // Limit each IP to 1000 requests per `window` (here, per 15 minutes).
   standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
